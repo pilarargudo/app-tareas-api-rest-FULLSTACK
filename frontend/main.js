1711f4406cd09a32e7b40d10e3ba6d81b3d2f5b1
Pilar Argudo <pargudo@disid.com>
2019-05-24 11:53:06 +0200

    Added delete task

--- frontend/main.js
+++ frontend/main.js
@@ -16,6 +16,7 @@ document.addEventListener( 'DOMContentLoaded', function () {
 
     }
 
+    // TODO para hacer columnas y enviar a la lista primera
     const appendTasks = tasksArray => {
         let tasksSection = document.querySelector( 'main' );
 
@@ -38,30 +39,53 @@ document.addEventListener( 'DOMContentLoaded', function () {
         // console.log(taskNode)
 
         // add listeners
-        addRemoveListener( taskNode );
+        // Añadimos taskObj.id para tener el id de todos los nodos
+        addRemoveListener( taskNode, taskObj.id );
         addCompleteListener( taskNode );
 
         return taskNode;
 
     }
 
+    // TODO modificar template
     let createTemplateHtmlString = ( { text, color, id, completed } ) =>
         `<div class="task ${completed ? 'completed': ''}" data-id="${id}" style="border-color: ${color}">
             <div class="text">${text}</div>
             <button class="remove">remove</button>
             <button class="complete">complete</button>
         </div>`
+
+// listeners  
+
+    // se incluye en add task (inputNode.addEventListener( 'keyup', function ( event ) {)
+    // se incluye en  create task (const createTaskNode = taskObj => {)
     let createNodeFromString = string => {
         let divNode = document.createElement( 'div' );
         divNode.innerHTML = string;
         return divNode.firstChild;
-    }
-    let addRemoveListener = node => {
+    } 
+    
+    // Obtenemos task.id para tener el id de todos los nodos
+    let addRemoveListener = (node, taskId) => {
+        
         node.querySelector( '.remove' ).addEventListener( 'click', event => {
+            // SOBRE EL DOM
             // event.target.parentNode.remove();
+            //node.remove();
+
+            // ejemplo de obtener atributo con js           
+            // const taskId = node.getAttribute("data-id");
+        
+            // llamada a la función
+             // y después de la promesa lo borramos del DOM
+            //delete_task(taskId).then( () => node.remove() ).catch('error')
+            delete_task(taskId);
             node.remove();
+
+
         } )
     }
+    
     let addCompleteListener = node => {
         node.querySelector( '.complete' ).addEventListener( 'click', event => {
             node.classList.toggle( 'completed' )
@@ -84,14 +108,14 @@ document.addEventListener( 'DOMContentLoaded', function () {
 
             .catch( console.error )
     }
-    // // add tasks
+
+    // add tasks
     let inputNode = document.querySelector( 'header input' );
 
     inputNode.addEventListener( 'keyup', function ( event ) {
         if ( event.keyCode === 13 ) {
             //get value from input
             let newTaskText = event.target.value;
-
 
             saveTaskToBackend( newTaskText ).then( () => {
                  // creat html string from value text
@@ -115,6 +139,27 @@ document.addEventListener( 'DOMContentLoaded', function () {
 
         }
     } )
+
+    // remove task
+    function delete_task(task_id) {
+
+        fetch(baseApiUrl + '/tasks', {
+                method: 'delete',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+
+                    task_id
+                })
+            })
+            .then(console.log)
+            .then(response => response.json())
+            .then(console.log)
+            .catch(console.error)
+
+    }
 
     // Encender la falla
     ////////////////////
